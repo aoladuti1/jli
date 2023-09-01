97822fc5b41f7653fb90c5c52e283c79b692d871
Antonio Oladuti <antoniooladuti3@hotmail.co.uk>
2023-09-01 19:30:59 +0100

    Clear tests and add some basic new ones

--- src/test/java/com/habu/AppTest.java
+++ src/test/java/com/habu/AppTest.java
@@ -4,60 +4,68 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        JPI.scanImport("com.habu.*");
-        JPI.scanImport("java.util.ArrayList");
-        ArrayList<Object> l = new ArrayList<>();
-        ArrayList<Object> l2 = new ArrayList<>();
+  /**
+   * Rigorous Test.
+   */
 
-        try {
-            byte f = 5;
-            BigDecimal x = new BigDecimal(f);
-            l.add(x);
+  static ArrayList<Object> args = new ArrayList<>();
 
-            Object o = JPI.call(Tester.class, "Tester", l);
-            System.out.println(o);
-            Object a = JPI.call(ArrayList.class, "ArrayList", l2);
-            Object b = Integer.valueOf(4);
-            l2.add(b);
-            JPI.call(a, "add", l2);
-            l2.clear();
-            l2.add(null);
-            JPI.call(a, "add", l2);
-            System.out.println(a);
-            JPI.call(JPI.getFieldOrInnerClass(JPI.call(Tester.class, "Tester", new ArrayList<>()).getClass(), "EV"), "c", new ArrayList<Object>());
+  ArrayList<Object> singleArg(Object o) {
+    args.clear();
+    args.add(o);
+    return args;
+  } 
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        assertTrue(true);
+  @Test
+  void testApp() {
+    Binder.scanImport("com.habu.*");
+    assertTrue(true);    
+  }
+
+  @Test
+  void testObjsAndNulls() { // null args should resolve to the object overload
+    assertFalse((boolean) Binder.call(Tester.class, "trueIfInt", singleArg(null)));
+  }
+
+  @Test 
+  void testConstruction() {
+    try {
+      assertTrue(Binder.call(Class.forName(Binder.getFullClassName("Tester")), null, args) != null);
+    } catch (Exception ex) {
+      ;
     }
+  }
 
-    @Test
-    void testObjsAndNulls() {
-        ArrayList<Object> ll = new ArrayList<>();
-        ArrayList<Object> nl = new ArrayList<>();
-        nl.add(null);
-        assertTrue(!((boolean) JPI.call(Tester.class, "ob", nl)));
-    }
+  @Test
+  void testEnum() { // null args should resolve to the object overload
+    assertTrue(Binder.getField(EnumTester.class, "OK") == EnumTester.OK);
+  }
 
-    @Test
-    void testCallNull() {
-        ArrayList<Object> nL = new ArrayList<>();
-        nL.add(5);
-        assertTrue((boolean) JPI.call(Tester.class, "ob", nL));
-    }
+  @Test
+  void testCallNull() {
+    assertTrue((boolean) Binder.call(Tester.class, "trueIfInt", singleArg(5)));
+  }
+
+  @Test
+  void testInnerConstructionAndRecasts() {
+    Tester t = new Tester();
+    Object inner1 = Binder.call(t, "InnerNoInt", singleArg(1));
+    Object inner2 = Binder.call(t, "InnerNoInt", singleArg(new BigDecimal(1.1)));
+    Binder.setRecastBigDecimals(false);
+    Object inner3 = Binder.call(t, "InnerNoInt", singleArg(new BigDecimal(1.1)));
+    Binder.setRecastBigDecimals(true);
+    assertTrue(inner1 == null && inner2 != null && inner3 == null);
+  }
 }
+
