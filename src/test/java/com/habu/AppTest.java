5db85255eac1f320b7dfe90e6cd1416bbad2250f
Antonio Oladuti <antoniooladuti3@hotmail.co.uk>
2024-05-10 11:47:19 +0100

    Adjust unit tests to fit the methods which throw exceptions

--- src/test/java/com/habu/AppTest.java
+++ src/test/java/com/habu/AppTest.java
@@ -1,15 +1,12 @@
 package com.habu;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
@@ -20,32 +17,41 @@ class AppTest {
    */
 
   static ArrayList<Object> args = new ArrayList<>();
+
   ArrayList<Object> singleArg(Object o) {
     args.clear();
     args.add(o);
     return args;
-  } 
+  }
 
   ArrayList<Object> noArgs() {
     args.clear();
     return args;
-  } 
+  }
 
   @Test
   void testApp() {
     Binder.scanImport("com.habu.*");
-    assertTrue(true);    
+    assertTrue(true);
   }
 
   @Test
   void testObjsAndNulls() { // null args should resolve to the object overload
-    assertFalse((boolean) Binder.call(Tester.class, "trueIfInt", singleArg(null)));
+    try {
+      assertFalse((boolean) Binder.call(Tester.class, "trueIfInt", singleArg(null)));
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
   }
 
-  @Test 
+  @Test
   void testConstruction() {
     Class<?> testerClass = Binder.forNameOrNull(Binder.getFullClassName("Tester"));
-    assertTrue(Binder.call(testerClass, "Tester", noArgs()) != null);
+    try {
+      assertTrue(Binder.call(testerClass, "Tester", noArgs()) != null);
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
   }
 
   @Test
@@ -56,42 +62,62 @@ class AppTest {
 
   @Test
   void testCallNull() {
-    assertTrue((boolean) Binder.call(Tester.class, "trueIfInt", singleArg(5)));
+    try {
+      assertTrue((boolean) Binder.call(Tester.class, "trueIfInt", singleArg(5)));
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
   }
 
   @Test
   void testInnerConstructionAndRecasts() {
+    Boolean success = true;
     Tester t = new Tester();
-    // passing an int should result in an error (null value)
-    Object inner1 = Binder.getMethod(t, "InnerNoInt", singleArg(1));
-    Object inner2 = Binder.call(t, "InnerNoInt", singleArg(new BigDecimal(1.1)));
+
+    try {
+      Binder.call(t, "InnerNoInt", singleArg(new BigDecimal(1.1)));
+    } catch (Exception ex) {
+      success = false;
+    }
     Binder.setRecastBigDecimals(false); // should result in an error (null value) now
-    Object inner3 = Binder.call(t, "InnerNoInt", singleArg(new BigDecimal(1.1)));
+    try {
+      Binder.call(t, "InnerNoInt", singleArg(new BigDecimal(1.1)));
+      success = false;
+    } catch (Exception ex) {
+      ;
+    }
     Binder.setRecastBigDecimals(true);
-    assertTrue(inner1 == null && inner2 != null && inner3 == null);
+    // passing an int should result in an error (null value)
+    Object inner = Binder.getMethod(t, "InnerNoInt", singleArg(1));
+    assertTrue(inner == null && success);
   }
 
   @Test
   void callStaticInnerMethod() {
     args.clear();
     Class<?> staticInner = (Class<?>) Binder.getFieldOrInnerClass(Tester.class, "StaticInner");
-    assertTrue((boolean) Binder.call(staticInner, "callMe", noArgs()));
+    try {
+      assertTrue((boolean) Binder.call(staticInner, "callMe", noArgs()));
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
   }
 
   @Test
   void varArgsTest() {
-    Binder.call(Tester.class, "varArgMethod", singleArg(new ArrayList<>()));
-    assertTrue(Tester.id == Tester.OBJARR);
+    try {
+      Binder.call(Tester.class, "varArgMethod", singleArg(new ArrayList<>()));
+      assertTrue(Tester.id == Tester.OBJARR);
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
   }
 
-  @Test 
+  @Test
   void importInnerClass() {
     String innerSimpleName = "InnerToImport";
     Binder.scanImport("com.habu.Tester.InnerToImport");
     assertTrue(Binder.simpleToFullNames.containsKey(innerSimpleName));
   }
 
-  
 }
-
-
